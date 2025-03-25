c9c8928a294fea4107be83f1744149f1166483bf
florcolazo <florantoc@gmail.com>
2025-03-24 21:13:59 -0300

    Agregando

---
+++ UADER_IS2_COLAZO/code/charts/line.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Datos de ejemplo
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 12, 18]
+
+# Crear la gráfica de líneas
+plt.plot(x, y, marker='o', linestyle='-')
+
+# Agregar títulos y etiquetas
+plt.title("Gráfico de Línea")
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+
+# Mostrar la gráfica
+plt.show()i
+x
